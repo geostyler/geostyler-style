9dd347d98464ddc61fa21190bad877d3ce8edd5c
Andreas Schmitz <schmitz@terrestris.de>
2025-06-04 15:37:30 +0200

    feat: update dependencies and switch to bun

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const compat = new FlatCompat({
+  baseDirectory: dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [{
+    files: [
+    "./*.ts"
+  ],
+}, ...compat.extends('@terrestris/eslint-config-typescript')];
