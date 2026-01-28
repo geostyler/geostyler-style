576d5f7f867d3c92cdfe59ee0e362a89c19c396c
jansule <suleiman@terrestris.de>
2026-01-28 14:59:29 +0100

    fix: reconstruct package structure
    BREAKING CHANGE: Changed structure of package. Version 10.2.0 includes
    an overlooked breaking change that changes the structure of the
    published package. Now, the original structure was recreated again. If
    you used any version prior 10.2.0, you do not have to change anything.
    If you used versions 10.2.0, 10.3.0, 10.4.0, or 10.5.0 you might have to
    readjust your imports, e.g. instead of importing via
    ```
    import { isFilter } from 'geostyler-style/dist/src/typeguards'
    ```
    you now can import via
    ```
    import { isFilter } from 'geostyler-style/dist/typeguards'
    ```
    again. Imports such as
    ```
    import { isFilter } from 'geostyler-style'
    ```
    were working throughout all mentioned versions and do not require any change.

--- vite.config.js
+++ vite.config.js
@@ -12,17 +12,26 @@ export default defineConfig({
   plugins: [
     dts({
       insertTypesEntry: true,
-      outDir: 'dist'
+      outDir: 'dist',
+      entryRoot: 'src',
+      compilerOptions: {
+        declarationDir: 'dist'
+      },
     }),
   ],
   build: {
     lib: {
       entry: 'src/index.ts',
-      name: 'geostyler-style',
       fileName: 'index',
       formats: ['es'],
     },
+    sourcemap: true,
     rollupOptions: {
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        entryFileNames: '[name].js',
+      },
       external: (id) => {
         return externalDeps.some(
           dep => id === dep || id.startsWith(`${dep}/`)
